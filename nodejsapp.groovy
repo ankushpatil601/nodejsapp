1b08b9d3c1e6daae4e974b4e74aefb3e4ed1e100
ankushpatil601 <50479288+ankushpatil601@users.noreply.github.com>
2019-09-16 17:29:33 +0530

    Update nodejsapp.groovy

--- nodejsapp.groovy
+++ nodejsapp.groovy
@@ -17,7 +17,7 @@ pipeline
 							//sh " cat /home/jenkins/.dockercfg "
 							//docker.withRegistry('https://index.docker.io/v1/', 'dockerhub') {
 						//def app = docker.build("nodeapp/docker-nodejs-demo:latest", '.').push()
-							sh 'docker login -u "ankushpa" -p "@nkush2019"  https://registry-1.docker.io/v2/'
+							sh 'sudo docker login -u "ankushpa" -p "@nkush2019"  https://registry-1.docker.io/v2/'
 						//}
 							
 										
