0c261b5c7e289e2d750785d37a9f96edd51205f9
ankushpatil601 <50479288+ankushpatil601@users.noreply.github.com>
2019-09-16 15:41:36 +0530

    Update nodejsapp.groovy

--- nodejsapp.groovy
+++ nodejsapp.groovy
@@ -13,10 +13,11 @@ pipeline
 						script
 						{
 							//checkout([$class: 'GitSCM', branches: [[name: "nodejsapp"]], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: "https://github.com/ankushpatil601/nodejsapp.git/nodejsapp"]]])
-                          				println "cat ~/.docker/config.json".execute().text
-							sh " cat /home/jenkins/.dockercfg "
-							docker.withRegistry('https://index.docker.io/v1/', 'dockerhub') {
+                          				//println "cat ~/.docker/config.json".execute().text
+							//sh " cat /home/jenkins/.dockercfg "
+							//docker.withRegistry('https://index.docker.io/v1/', 'dockerhub') {
 						//def app = docker.build("nodeapp/docker-nodejs-demo:latest", '.').push()
+							sh 'docker login -u "ankushpa" PASSWORD "@nkush2019"  https://index.docker.io/v1/'
 						}
 							
 							
