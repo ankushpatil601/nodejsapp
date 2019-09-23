7b9290ceb6c04aad84bf84cf0ee955ec1009ab4c
ankushpatil601 <50479288+ankushpatil601@users.noreply.github.com>
2019-09-23 18:37:25 +0530

    Update nodejsapp.groovy

--- nodejsapp.groovy
+++ nodejsapp.groovy
@@ -19,7 +19,7 @@ pipeline
 							def app = docker.build("ankushpa/nodeapp:19", '.')
 								app.push()
 								app.pull()
-								app.run("5000:8080")
+								app.run().withRun('-p 5000:8080')
 								//docker pull nodeapp
 							// docker images
 								//app.run(nodeapp)
